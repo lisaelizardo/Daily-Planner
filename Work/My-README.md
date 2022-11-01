d2abd5585e3cd84387f15f0bc2c880159b0d6688
Deneene Whitaker <lisaelizrdo@gmail.com>
2022-11-01 16:30:14 -0500

    Fixed date and added some time elements

---
+++ Work/My-README.md
@@ -0,0 +1,4 @@
+This Daily Planner has normal business hours that the user can add comments into the 
+text area for the hour(s) and save them to their local storage.
+
+The timeblocks are color coded to represent the Past, Present, and Future hour(s).
--- Work/script.js
+++ Work/script.js
@@ -1,3 +1,24 @@
-var today = moment('2022-10-29T15:02:00');
+var today = moment();
+var currentTime = moment().hour();
+var timeId = moment().
+//formats current date to Month Day, Year
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+//adds colors to past, present, and future hours-
+//depending on time of day
+if (timeId === currentTime) {
+    $(this).addClass("present");
+} else if (timeId < currentTime) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+} else (timeId > currentTime); {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future"); }
+
+//$(currentTime).class("past");(add/remove?)
+
+//$(currentTime).class("present");
+
+//$(currentTime).class("future");
